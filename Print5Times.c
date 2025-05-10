ef19055ec1793674c7522667c7e5d648b2203305
RAM121-LB <ramthakare2006@gmail.com>
2025-05-10 22:20:17 +0530

    Add files via upload

---
+++ Print5Times.c
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////
+//
+//  File name :     Print5Time.c
+//  Descreption :   Used to print 5 Times "Marvellous" on screen
+//  Author :        Ram Madhukar Thakare
+//  Date :          10/05/2025
+//
+///////////////////////////////////////////////////////
+
+#include <stdio.h>
+void Display()
+{
+    int i = 0;
+    for(i = 1;i <= 5; i++)
+    {
+        printf("Marvellous\n");
+    }    
+}
+int main()
+{
+    Display();
+    return 0;
+}
+
+/* Output :
+Marvellous
+Marvellous
+Marvellous
+Marvellous
+Marvellous
+*/ 
