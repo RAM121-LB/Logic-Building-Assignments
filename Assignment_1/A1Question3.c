e007a8088f4c8f646df1164a17e54dd328683afc
RAM121-LB <ramthakare2006@gmail.com>
2025-05-14 13:46:31 +0530

    Add files via upload

---
+++ Assignment_1/A1Question3.c
@@ -0,0 +1,34 @@
+///////////////////////////////////////////////////////
+//
+//  File name :     A1Question3.c
+//  Descreption :   Used to print 5 to 1 on screen
+//  Author :        Ram Madhukar Thakare
+//  Date :          10/05/2025
+//
+///////////////////////////////////////////////////////
+
+#include<stdio.h>
+void Display()
+{
+    int i = 0;
+    i = 5;
+    while(i >= 1)
+    {
+        printf("%d\n",i);
+        i--;
+    }
+}
+
+int main()
+{
+    Display();
+}
+
+/*
+Output : 
+5
+4
+3
+2
+1
+*/
